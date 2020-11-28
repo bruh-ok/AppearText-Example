79a39b2ad5a9890c07bd1280cae595685348d322
TSX <justcarmeetsfive@gmail.com>
2020-11-28 12:54:20 +0000

    Finalised, Working Build

---
+++ hover/animate.js
@@ -0,0 +1,14 @@
+window.addEventListener('load', () => {
+    document.getElementsByClassName('appear-overall')[0].addEventListener('mouseover', () => {
+        document.getElementsByClassName('appear-container-first')[0].classList.add('appear-container-first-changed')
+        setTimeout(() => {
+            document.getElementsByClassName('appear-container-second')[0].classList.add('appear-container-second-changed')
+        }, 1000);
+    })
+    document.getElementsByClassName('appear-overall')[0].addEventListener('mouseout', () => {
+        document.getElementsByClassName('appear-container-first')[0].classList.remove('appear-container-first-changed')
+        setTimeout(() => {
+            document.getElementsByClassName('appear-container-second')[0].classList.remove('appear-container-second-changed')
+        }, 1000);
+    })
+})
