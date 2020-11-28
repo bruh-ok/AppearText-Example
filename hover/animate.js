a3e3586deb747abad8895a3662de8d0053c1c4e9
YaBoiTZ <justcarmeetsfive@gmail.com>
2020-11-28 16:12:02 +0000

    Little tweaks | Working Build.

--- hover/animate.js
+++ hover/animate.js
@@ -2,7 +2,11 @@ window.addEventListener('load', () => {
     document.getElementsByClassName('appear-overall')[0].addEventListener('mouseover', () => {
         document.getElementsByClassName('appear-container-first')[0].classList.add('appear-container-first-changed')
         setTimeout(() => {
-            document.getElementsByClassName('appear-container-second')[0].classList.add('appear-container-second-changed')
+            if(document.getElementsByClassName('appear-container-first')[0].classList.contains('appear-container-first-changed')) {
+                document.getElementsByClassName('appear-container-second')[0].classList.add('appear-container-second-changed')   
+            } else {
+                return
+            }
         }, 1000);
     })
     document.getElementsByClassName('appear-overall')[0].addEventListener('mouseout', () => {
